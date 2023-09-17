04d81afa0454b60e8387520a3335884e799f05a8
BinhLH <Laxus.predator@gmail.com>
2023-09-17 17:19:36 +0700

    test env variable

--- src/main/java/com/example/MyOwnMP3/Common/Const.java
+++ src/main/java/com/example/MyOwnMP3/Common/Const.java
@@ -1,9 +1,21 @@
 package com.example.MyOwnMP3.Common;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Const {
-    public static String FPT_ADDRESS = "files.000webhost.com";
+
+
+    public static String FTP_ADDRESS = "files.000webhost.com";
     public static String DATABASE_NAME = "musicdb028";
-    public static String FPT_PW = "meomuop2812";
-    public static String FPT_MUSIC_DIR = "/public_html/music/";
+
+    public static String FTP_PW;
+    @Value("${testpw}")
+    public void setFptPw(String ftpPw) {
+        FTP_PW = ftpPw;
+    }
+
+    public static String FTP_MUSIC_DIR = "/public_html/music/";
     public static String MUSIC_DIR = "https://musicdb028.000webhostapp.com/music/";
 }
